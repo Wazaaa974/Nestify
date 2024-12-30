ce158e3ea573ef6f61d3743b0b3979ff574b272e
Julie Simon <sim.ju@live.fr>
2024-12-30 21:06:48 +0100

    carousel en cours

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,44 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+$(document).ready(function () {
+  var $carouselInner = $('.carousel-inner');
+  var $carouselItems = $('.carousel-item');
+  var cardWidth = $carouselItems.width();
+  var scrollPosition = 0;
+
+  // Clone les premiers et derniers éléments pour créer une boucle
+  $carouselInner.append($carouselItems.clone());
+  $carouselInner.prepend($carouselItems.clone());
+
+  // Ajuste la position initiale pour éviter un saut au début
+  var initialOffset = $carouselItems.length * cardWidth;
+  $carouselInner.scrollLeft(initialOffset);
+  scrollPosition = initialOffset;
+
+  // Bouton "next"
+  $('.carousel-control-next').on('click', function () {
+    scrollPosition += cardWidth;
+    $carouselInner.animate({ scrollLeft: scrollPosition }, 600, function () {
+      // Réinitialise si on dépasse la fin des clones
+      if (scrollPosition >= (initialOffset + $carouselItems.length * cardWidth)) {
+        scrollPosition = initialOffset;
+        $carouselInner.scrollLeft(scrollPosition);
+      }
+    });
+  });
+
+  // Bouton "prev"
+  $('.carousel-control-prev').on('click', function () {
+    scrollPosition -= cardWidth;
+    $carouselInner.animate({ scrollLeft: scrollPosition }, 600, function () {
+      // Réinitialise si on dépasse le début des clones
+      if (scrollPosition <= 0) {
+        scrollPosition = initialOffset + ($carouselItems.length - 1) * cardWidth;
+        $carouselInner.scrollLeft(scrollPosition);
+      }
+    });
+  });
+});
+
